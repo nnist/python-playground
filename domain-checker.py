302bfb2cc9a43ee3ac005bbad23209441d52b8b9
Nicole <n.nisters@gmail.com>
2017-08-24 14:41:02 +0200

    Add top-level domain argument

--- domain-checker.py
+++ domain-checker.py
@@ -9,16 +9,18 @@ import itertools
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"hl:",["length="])
+        opts, args = getopt.getopt(argv,"hl:t:",["length=","tld="])
     except getopt.GetoptError:
-        print('usage: domain-checker.py -l <length>')
+        print('usage: domain-checker.py -l <length> -t <tld>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('usage: domain-checker.py -l <length>')
+            print('usage: domain-checker.py -l <length> -t <tld>')
             sys.exit()
         elif opt in ("-l", "--length"):
             length = int(arg)
+        elif opt in ("-t", "--tld"):
+            tld = str(arg)
 
     # Build list of tuples containing alphabet
     probs = ()
@@ -28,9 +30,10 @@ def main(argv):
     # Build list of domains
     domains = []
     for prob in itertools.product(*probs):
-        print(prob)
-        domains += ''.join(prob)
+        domain = ''.join(prob) + tld
+        domains.append(domain)
 
+    print(domains)
     print("Checking %s domains..." % len(domains))
 
 if __name__ == "__main__":
