92981155887555fb1dd5bf713511b7f8817b0cc7
Nicole <n.nisters@gmail.com>
2017-08-26 12:43:05 +0200

    Remove TODOs

--- domain-name-tool/domain-name-tool.py
+++ domain-name-tool/domain-name-tool.py
@@ -1,7 +1,4 @@
 # https://en.wikipedia.org/wiki/Domain_hack
-# TODO Load dictionary
-# TODO Create list of TLDs
-# TODO Find words ending with TLD (.de, .se) like co.de or comato.se
 
 import string
 from tqdm import *
