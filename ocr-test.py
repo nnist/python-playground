254329b0698d6b9d88688a6557d3cf1bf3e9f07d
Nicole <n.nisters@gmail.com>
2018-04-04 18:27:50 +0200

    Clean up old scripts

--- ocr-test.py
+++
@@ -1,7 +0,0 @@
-try:
-    import Image
-except ImportError:
-    from PIL import Image
-import pytesseract
-print(pytesseract.image_to_string(Image.open('images/00006.png'), lang='eng'))
-# print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
--- progress_indicator.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-progress = 0
-while progress < 100:
-    sys.stdout.write('\r[%s%s]' % ('=' * progress, ' ' * (50 - progress)))
-    sys.stdout.flush()
-    progress += 1
