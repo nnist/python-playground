a7db817aaab42620bcc40f14e99f3b560a3e446b
Nicole <n.nisters@gmail.com>
2018-04-23 11:49:32 +0200

    Colorize output

--- p2000-scraper/stats.py
+++ p2000-scraper/stats.py
@@ -10,22 +10,42 @@ def query_database(query):
     cur.close()
     return results
 
+def pretty_print(rows):
+    for i in range(len(rows)):
+        row = []
+        for ii in range(len(rows[i])):
+            item = rows[i][ii]
+            if item == "Brandweer":
+                item = "\033[31m" + item + "\033[0m"
+            elif item == "Ambulance":
+                item = "\033[32m" + item + "\033[0m"
+            elif item == "Politie":
+                item = "\033[34m" + item + "\033[0m"
+
+            if ii == 2: # Region
+                item = "\033[33m" + item + "\033[0m"
+            elif ii == 5: # Details
+                item = "\033[1m" + item + "\033[0m"
+            row.append(item)
+        rows[i] = row
+    print(tabulate(rows))
+
 # results = query_database("""SELECT * FROM messages WHERE capcodes NOT NULL"""))
 # results = query_database("""SELECT * FROM messages WHERE type='Brandweer'""")
 
 # Get most recent message
-results = query_database("""SELECT MAX(date_time), type, region, details
+results = query_database("""SELECT MAX(date_time), type, region, priority, postcode, details
                             FROM messages
                         """)
 print('\nMost recent message')
-print(tabulate(results))
+pretty_print(results)
 
 # Get first message
-results = query_database("""SELECT MIN(date_time), type, region, details
+results = query_database("""SELECT MIN(date_time), type, region, priority, postcode, details
                             FROM messages
                         """)
 print('\nFirst message')
-print(tabulate(results))
+pretty_print(results)
 
 # Count all call types
 results = query_database("""SELECT type, COUNT(*)
@@ -33,7 +53,7 @@ results = query_database("""SELECT type, COUNT(*)
                             GROUP BY type
                         """)
 print('\nNumber of calls per type')
-print(tabulate(results))
+pretty_print(results)
 
 # Return number of messages per region
 results = query_database("""SELECT region, COUNT(*) AS cnt
@@ -48,4 +68,4 @@ print(tabulate(results))
 dt = datetime.today() - timedelta(minutes=30)
 results = query_database("SELECT date_time, type, region, priority, postcode, details FROM messages WHERE date_time > '" + str(dt) + "' ORDER BY date_time ASC")
 print('\nAll messages of last half hour')
-print(tabulate(results))
+pretty_print(results)
