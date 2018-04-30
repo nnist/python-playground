00f8871df93383e80bac125f0b6d03f9e7501e91
Nicole <n.nisters@gmail.com>
2018-05-01 01:54:03 +0200

    Add tests for min and max word length

--- passphrase-tool/test_passphrase_tool.py
+++ passphrase-tool/test_passphrase_tool.py
@@ -4,3 +4,15 @@ def test_number_of_words():
     generator = PassphraseGenerator(length_min=9, length_max=9, number=12)
     result = generator.generate()
     assert len(result) == 12
+
+def test_minimum_length():
+    generator = PassphraseGenerator(length_min=7, length_max=12, number=500)
+    results = generator.generate()
+    for result in results:
+        assert len(result) >= 7
+
+def test_maximum_length():
+    generator = PassphraseGenerator(length_min=7, length_max=12, number=500)
+    results = generator.generate()
+    for result in results:
+        assert len(result) <= 12
