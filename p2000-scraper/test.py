834dfbf69f06c8c2402dd622f8df2fc8ffb45d6b
Nicole <n.nisters@gmail.com>
2018-05-07 11:55:26 +0200

    Add test for p2000 scraper

---
+++ p2000-scraper/test.py
@@ -0,0 +1,16 @@
+import pytest
+from scraper import *
+
+@pytest.mark.timeout(10)
+def test_scraper_get_page():
+    scraper = Scraper(1, 0, 1)
+    html, status = scraper.get_page("http://www.p2000-online.net/p2000.php?\
+                                     Pagina=0&AutoRefresh=uit")
+    print(html)
+    assert html is not None and status is 200
+
+@pytest.mark.timeout(10)
+def test_scraper():
+    scraper = Scraper(1, 0, 1)
+    scraper.scrape_page("http://www.p2000-online.net/p2000.php?Pagina=0&AutoRefresh=uit")
+    assert True
