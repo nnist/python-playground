567bdc05644380f71993453da165c80d827f6559
Nicole <n.nisters@gmail.com>
2018-05-14 11:49:06 +0200

    Present words in a better way

--- passphrase-tool/app/routes.py
+++ passphrase-tool/app/routes.py
@@ -3,9 +3,8 @@
 # TODO Add short useful info about passphrases, with sources
 # TODO Checkboxes for verbs, nouns, etc.
 # TODO Add reset button to form
-# TODO Present passphrase words in a better way
 
-from flask import render_template
+from flask import render_template, Markup
 from app import app
 from passphrase_tool import PassphraseGenerator
 from flask_wtf import FlaskForm
@@ -64,7 +63,12 @@ def index():
     generator = PassphraseGenerator(length_min, length_max, double, adjecent,
                                     allowed_chars, dict_file, number)
     results = generator.generate()
-    words = {'words': str(results)}
+    
+    # Format and display words
+    words = ""
+    for word in results:
+        words += word + "<br>"
+    words = Markup(words)
 
     return render_template('index.html', title='Passphrase tool', words=words,
                            options_form=options_form)
