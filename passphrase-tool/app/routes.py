23be43a807bb28593e862623b567eafeab6dfd8b
Nicole <n.nisters@gmail.com>
2018-05-14 09:22:07 +0200

    Remove trailing whitespaces

--- passphrase-tool/app/routes.py
+++ passphrase-tool/app/routes.py
@@ -32,7 +32,7 @@ def index():
     number = 12
 
     options_form = OptionsForm()
-    
+
     min_length_data = options_form.min_length.data
     if min_length_data is '' or min_length_data is None:
         options_form.min_length.data = length_min
@@ -44,16 +44,16 @@ def index():
         options_form.max_length.data = length_max
     else:
         length_max = int(max_length_data)
-    
+
     allowed_chars_data = options_form.allowed_chars.data
     if allowed_chars_data is '' or allowed_chars_data is None:
         options_form.allowed_chars.data = allowed_chars
     else:
         allowed_chars = allowed_chars_data
-    
+
     double = options_form.double.data
     adjecent = options_form.adjecent.data
-    
+
     if not options_form.validate_on_submit():
         print('error: form not valid')
         # TODO Properly handle this
@@ -61,5 +61,5 @@ def index():
     generator = PassphraseGenerator(length_min, length_max, double, adjecent, allowed_chars, dict_file, number)
     results = generator.generate()
     words = {'words': str(results)}
-    
+
     return render_template('index.html', title='Passphrase tool', words=words, options_form=options_form)
