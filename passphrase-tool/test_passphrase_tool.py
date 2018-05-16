d4929d9ab8e276aafc6f72fa622a71a366b13754
Nicole <n.nisters@gmail.com>
2018-05-16 10:12:55 +0200

    Add docstrings

--- passphrase-tool/test_passphrase_tool.py
+++ passphrase-tool/test_passphrase_tool.py
@@ -1,3 +1,4 @@
+"""Test the passphrase tool and the web interface."""
 from passphrase_tool import *
 import urllib.request
 from app import app
@@ -5,28 +6,33 @@ import pytest
 
 @pytest.fixture
 def client():
+    """Create a client for testing the web interface."""
     app.config['TESTING'] = True
     client = app.test_client()
     return client
 
 def test_number_of_words():
+    """Test the number of words."""
     generator = PassphraseGenerator(length_min=9, length_max=9, number=12)
     result = generator.generate()
     assert len(result) == 12
 
 def test_minimum_length():
+    """Test the minimum length of the words."""
     generator = PassphraseGenerator(length_min=7, length_max=12, number=500)
     results = generator.generate()
     for result in results:
         assert len(result) >= 7
 
 def test_maximum_length():
+    """Test the maximum length of the words."""
     generator = PassphraseGenerator(length_min=7, length_max=12, number=500)
     results = generator.generate()
     for result in results:
         assert len(result) <= 12
 
 def test_invalid_input():
+    """Test for invalid input."""
     generator = PassphraseGenerator(length_min=7, length_max=6, number=500)
     results = generator.generate()
     assert results == ["Error: Invalid input"]
@@ -36,5 +42,6 @@ def test_invalid_input():
     assert results == ["Error: Dict file does not exist"]
 
 def test_webserver_running(client):
+    """Test to see if the webserver is running."""
     rv = client.get('/')
     assert rv.data != b'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">\n<title>404 Not Found</title>\n<h1>Not Found</h1>\n<p>The requested URL was not found on the server.  If you entered the URL manually please check your spelling and try again.</p>\n'
