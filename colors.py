d1b1c311b23cf8b3f8f17a7718b8b5cf99dc31d9
Nicole <n.nisters@gmail.com>
2018-05-28 15:12:16 +0200

    Fix missing whitespaces

--- colors.py
+++ colors.py
@@ -1,6 +1,6 @@
 from tabulate import tabulate
 
-selection = [1,2,3,4,7,8] + list(range(30, 48)) + list(range(90, 108))
+selection = [1, 2, 3, 4, 7, 8] + list(range(30, 48)) + list(range(90, 108))
 results = [('', '    ', 'normal', '████', '')]
 results = [('0', '    ', 'end', '', '\\033[0m')]
 for i in selection:
