88bd18bf8ebe54fd89b7b8233e0c2a5dce54a3a2
Nicole <n.nisters@gmail.com>
2018-05-28 15:11:18 +0200

    Split too long line

--- colors.py
+++ colors.py
@@ -5,5 +5,8 @@ results = [('', '    ', 'normal', '████', '')]
 results = [('0', '    ', 'end', '', '\\033[0m')]
 for i in selection:
     #print('\033[%sm\\033[%sm \\033[0m\033[0m' % (i, i))
-    results.append((i, '\033[%sm    \033[0m' % i, '\033[%smexample\033[0m' % i, '\033[%sm████\033[0m' % i, '\033[%sm\\033[%sm\033[0m' % (i, i)))
+    results.append((i, '\033[%sm    \033[0m' % i,
+                    '\033[%smexample\033[0m' % i,
+                    '\033[%sm████\033[0m' % i,
+                    '\033[%sm\\033[%sm\033[0m' % (i, i)))
 print(tabulate(results))
