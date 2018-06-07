1934c0c38e483d7e3d9bd941768267ea889afb0a
Nicole <n.nisters@gmail.com>
2018-06-07 09:49:31 +0200

    Add unit tests

---
+++ domain-name-tool/test.py
@@ -0,0 +1,19 @@
+"""Test the domain name tool."""
+import pytest
+from domain-name-tool import DomainChecker
+
+def test_get_domains():
+    """Test the get_domains method."""
+    assert False
+
+def test_check_domain():
+    """Test the check_domain method."""
+    assert False
+
+def test_check_domains():
+    """Test the check_domains method."""
+    assert False
+
+def test_cli():
+    """Test the cli."""
+    assert False
