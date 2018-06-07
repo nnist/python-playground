b06ae77ae1de5899635ac56cab3427e8b51ae630
Nicole <n.nisters@gmail.com>
2018-06-07 09:44:56 +0200

    Make object oriented

--- domain-name-tool/domain-name-tool.py
+++ domain-name-tool/domain-name-tool.py
@@ -8,27 +8,85 @@ import os
 import time
 import subprocess
 
-def check_domain(domain):
-    """Do a whois request on domain. Return the domain's status."""
-    try:
-        process = subprocess.run(['whois', domain], timeout=1,
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE, check=True,
-                                 universal_newlines=True)
-        output = process.stdout
-        if 'NOT AVAILABLE' in output or 'NOT ALLOWED' in output\
-        or 'active' in output:
-            return 'not_available'
-        elif 'NOT FOUND' in output or 'AVAILABLE' in output\
-        or 'is free' in output:
-            return 'available'
-        elif 'exceeded' in output:
-            return 'throttled'
-        return 'unknown'
-    except subprocess.CalledProcessError:
-        return 'error'
-    except subprocess.TimeoutExpired:
-        return 'timeout'
+class DomainChecker():
+    """Get domain hacks from a dictionary and test them for availability."""
+    def __init__(self, length_min, length_max, tld, dict_file, chars, delay):
+        self.length_min = length_min
+        self.length_max = length_max
+        self.tld = tld
+        self.dict_file = dict_file
+        self.chars = chars
+        self.delay = delay
+
+    def get_domains(self):
+        """Load dictionary and return lines ending with tld"""
+        domains = []
+        with open(self.dict_file) as f:
+            for line in f:
+                line = line[0:-1].lower()
+                if(line.endswith(self.tld)) and '-' not in line:
+                    if len(line) >= self.length_min\
+                    and len(line) <= self.length_max:
+                        domain = line[0:-(len(self.tld))] + "." + self.tld
+                        domains.append(domain)
+        return domains
+
+    def check_domains(self, domains):
+        """Check a list of domains for availability."""
+        print("Checking %s domains..." % len(domains))
+
+        f = open('log.txt', 'a')
+        f.write('\n{}\n'.format(datetime.datetime.now()))
+
+        # Try to get whois information for domain to see if it is available
+        for i in range(len(domains)):
+            print("[" + str(i+1) + "/" + str(len(domains)) + "] " +
+                  domains[i], end=" -> ", flush=True)
+            domain = domains[i]
+            status = self.check_domain(domain)
+            if status == 'not_available':
+                print("\033[31mnot available\033[0m")
+                f.write('{} is not available\n'.format(domains[i]))
+            elif status == 'available':
+                print("\033[32mavailable\033[0m")
+                f.write('{} is available\n'.format(domains[i]))
+            elif status == 'throttled':
+                print('throttled')
+            elif status == 'unknown':
+                print("\033[33munknown\033[0m")
+                f.write('{} might be available\n'.format(domains[i]))
+            elif status == 'error':
+                print('error')
+            elif status == 'timeout':
+                print('timeout')
+
+            time.sleep(self.delay)
+
+        f.close()
+
+    def check_domain(self, domain):
+        """Do a whois request on domain. Return the domain's status."""
+        try:
+            process = subprocess.run(['whois', domain], timeout=1,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE, check=True,
+                                     universal_newlines=True)
+            output = process.stdout
+            if 'NOT AVAILABLE' in output or 'NOT ALLOWED' in output\
+            or 'active' in output:
+                return 'not_available'
+            elif 'NOT FOUND' in output or 'AVAILABLE' in output\
+            or 'is free' in output:
+                return 'available'
+            elif 'exceeded' in output:
+                return 'throttled'
+
+            print(process.stdout)
+            return 'unknown'
+        except subprocess.CalledProcessError:
+            return 'error'
+        except subprocess.TimeoutExpired:
+            return 'timeout'
 
 def main(argv):
     parser = argparse.ArgumentParser(
@@ -69,46 +127,10 @@ def main(argv):
     chars = "abcdefghijklmnopqrstuvwxyz"
     delay = args.delay
 
-    # Load dictionary, check for lines ending with tld
-    domains = []
-    with open(dict_file) as f:
-        for line in f:
-            line = line[0:-1].lower()
-            if(line.endswith(tld)) and '-' not in line:
-                if len(line) >= length_min and len(line) <= length_max:
-                    domain = line[0:-(len(tld))] + "." + tld
-                    domains.append(domain)
-
-    print("Checking %s domains..." % len(domains))
-
-    f = open('log.txt', 'a')
-    f.write('\n{}\n'.format(datetime.datetime.now()))
-
-    # Try to get whois information for domain to see if it is available or not
-    for i in range(len(domains)):
-        print("[" + str(i+1) + "/" + str(len(domains)) + "] " + domains[i], end=" -> ", flush=True)
-        domain = domains[i]
-        status = check_domain(domain)
-        if status == 'not_available':
-            print("\033[31mnot available\033[0m")
-            f.write('{} is not available\n'.format(domains[i]))
-        elif status == 'available':
-            print("\033[32mavailable\033[0m")
-            f.write('{} is available\n'.format(domains[i]))
-        elif status == 'throttled':
-            print('throttled')
-        elif status == 'unknown':
-            print("\033[33munknown\033[0m")
-            f.write('{} might be available\n'.format(domains[i]))
-            print(process.stdout)
-        elif status == 'error':
-            print('error')
-        elif status == 'timeout':
-            print('timeout')
-
-        time.sleep(delay)
-
-    f.close()
+    domain_checker = DomainChecker(length_min, length_max, tld, dict_file,
+                                   chars, delay)
+    domains = domain_checker.get_domains()
+    domain_checker.check_domains(domains)
 
 if __name__ == "__main__":
     try:
